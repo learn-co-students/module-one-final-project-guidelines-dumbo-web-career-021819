9a41f55aef5b52b5cb314e930632d98a762ca11b
James Dorr <jamesdorr3@gmail.com>
2019-03-04 17:14:11 -0500

    laid out methods to create in list, shopper

--- lib/list.rb
+++ lib/list.rb
@@ -1,4 +1,15 @@
 class List < ActiveRecord::Base
   belongs_to :shopper
   has_many :lists_ingredients
+
+  def add_item(item)
+    #find_or_create_by
+  end
+
+  def delete_item(item)
+  end
+
+  def check_item(item)
+  end
+
 end
--- lib/shopper.rb
+++ lib/shopper.rb
@@ -2,13 +2,17 @@ class Shopper < ActiveRecord::Base
   has_many :lists
   has_many :lists_ingredients, through: :list
 
-#hjkhjkhjkhjkhkjhk
+  def add_item(list, item)
+    #find_or_create_by
+  end
 
+  def create_list(name)
+  end
 
+  def check_item(list, item)
+  end
 
-
-
-
-
+  def delete_item(list, item)
+  end
 
 end
