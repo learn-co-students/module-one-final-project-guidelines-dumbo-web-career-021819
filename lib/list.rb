8d0c81621c619e6af56db6ba9f3f29659536181c
James Dorr <jamesdorr3@gmail.com>
2019-03-05 10:28:11 -0500

    downcase, planned methods

--- lib/list.rb
+++ lib/list.rb
@@ -3,18 +3,22 @@ class List < ActiveRecord::Base
   belongs_to :shopper
   has_many :list_items
 
+  ################ .downcase all List and Item names! ##############
+
   def add_item(name)
-    item = Item.find_or_create_by(name: name)
-    list_item = ListItem.find_or_create_by(list_id: self.id, item_id: item.id)
+    item = Item.find_or_create_by(name: name.downcase)
+    list_item = ListItem.find_or_create_by(list_id: self.id, item_id: item.id, still_needed: true)
     "#{item.name.capitalize} - added to your list: #{self.name}!"
   end
-
-
 
   def delete_item(item)
   end
 
   def check_item(item)
+  end
+
+  def items
+    #lists all of the items
   end
   # binding.pry
 end
