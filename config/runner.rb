e575b9b68571ac15fa6fbd9affec7fbb8fde0eb8
merestrickland <mere.strickland@gmail.com>
2019-03-06 18:59:24 -0500

    wanted to murder/suicide my computer

--- config/runner.rb
+++ config/runner.rb
@@ -1,7 +1,18 @@
 require_relative '../config/environment.rb'
 
+
+Shopper.destroy_all
+ListItem.destroy_all
+List.destroy_all
+
+
+
 carla = Shopper.create(name: "Carla")
 partylist = carla.create_list("party")
+supperlist = carla.create_list("supper")
+
+
+
 
 partylist.add_item('beer')
 partylist.add_item('potatoes')
@@ -9,6 +20,9 @@ partylist.add_item('butter')
 partylist.delete_item('butter')
 partylist.add_item('streamers')
 partylist.check_item('streamers')
+carla.delete_list("supper")
+
+
 
 
 
--- lib/shopper.rb
+++ lib/shopper.rb
@@ -5,16 +5,25 @@ class Shopper < ActiveRecord::Base
 
     ################ .downcase Item names! ##############
 
-  def create_list(list_name) # it works!
-    new_list = List.create(name: list_name, shopper_id: self.id)
+  def create_list(list_name) #creates a new list
+    List.create(name: list_name, shopper_id: self.id)
   end
 
-  def delete_shopper # Should it also delete all associated ListItems?
-    self.delete
+  def delete_list(name) #doesn't work in console, but works from runner file
+    # puts "im in the method"
+    # binding.pry
+    self.lists.find_by(name: name).destroy
   end
 
-  def list_names
-    #shows array of names of all lists the shopper has
+  def delete_shopper # Should it also delete all associated ListItems? YES 
+    self.list_items.each do |list_item|
+      list_item.destroy
+    end
+    # self.lists.destroy
+    # self.destroy
+  end
+
+  def list_names #returns an array of all list names of shopper
     self.lists.map(&:name)
   end
 
