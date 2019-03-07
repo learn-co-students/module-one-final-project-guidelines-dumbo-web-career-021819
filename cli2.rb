4613cdd5ab133d7acc64dff3b9ee931c47c653b4
James Dorr <jamesdorr3@gmail.com>
2019-03-07 11:57:46 -0500

    TTY select-name and -list

--- cli2.rb
+++ cli2.rb
@@ -14,9 +14,23 @@ def sign_up
   prompt = TTY::Prompt.new
   prompt.ask("What is your name?", required: true)
 end
- 
-username = sign_up
 
+def select_name
+  prompt = TTY::Prompt.new
+  prompt.select("Select your name.", Shopper.all.map(&:name))
+end
+
+@username = select_name
+
+def select_list
+  prompt = TTY::Prompt.new
+  user = Shopper.all.find_by(name: @username)
+  userlists = user.lists.map(&:name)
+  userlists << "-Add New List-"
+  prompt.select("Select a list.", userlists)
+end
+
+select_list
 
 #
 # def flow
@@ -53,9 +67,7 @@ def tty_check_item_on_list
   prompt.select("What item would you like to mark?", items)
 end
 
-
 # userlist.add_item(tty_add_item_to_list)
 # tty_check_item_on_list
 # tty_add_item_to_list
 # tty_delete_item_from_list
-
